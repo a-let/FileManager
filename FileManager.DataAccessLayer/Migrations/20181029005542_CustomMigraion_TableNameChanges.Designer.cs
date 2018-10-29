bed4e020e594bde0b2e1d1a246266d9a69c5e1b2
a-let <aletinski@yahoo.com>
2018-10-28 18:00:47 -0700

    Renamed tables via EF. Episodes -> Episode, Movies -> Movie, Seasons -> Season, Shows -> Shows (#19)

--- FileManager.DataAccessLayer/Migrations/20181029005542_CustomMigraion_TableNameChanges.Designer.cs
+++ FileManager.DataAccessLayer/Migrations/20181029005542_CustomMigraion_TableNameChanges.Designer.cs
@@ -3,14 +3,16 @@ using FileManager.DataAccessLayer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace FileManager.DataAccessLayer.Migrations
 {
     [DbContext(typeof(FileManagerContext))]
-    partial class FileManagerContextModelSnapshot : ModelSnapshot
+    [Migration("20181029005542_CustomMigraion_TableNameChanges")]
+    partial class CustomMigraion_TableNameChanges
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ FileManager.DataAccessLayer/Migrations/20181029005542_CustomMigraion_TableNameChanges.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FileManager.DataAccessLayer.Migrations
+{
+    public partial class CustomMigraion_TableNameChanges : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameTable(name: "Episodes", newName: "Episode");
+            migrationBuilder.RenameTable(name: "Movies", newName: "Movie");
+            migrationBuilder.RenameTable(name: "Seasons", newName: "Season");
+            migrationBuilder.RenameTable(name: "Shows", newName: "Show");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameTable(name: "Shows", newName: "Show");
+            migrationBuilder.RenameTable(name: "Seasons", newName: "Season");
+            migrationBuilder.RenameTable(name: "Movies", newName: "Movie");
+            migrationBuilder.RenameTable(name: "Episode", newName: "Episodes");
+        }
+    }
+}
