52d0f358ca6993661082e7c8c51ccddd260e224b
Alex Letinski <aletinski@yahoo.com>
2018-11-08 14:18:15 -0800

    LogTable Migration

---
+++ FileManager.Models/Log.cs
@@ -0,0 +1,10 @@
+﻿namespace FileManager.Models
+{
+    public class Log
+    {
+        public int LogId { get; set; }
+        public string LogLevel { get; set; } 
+        public string Message { get; set; }
+        public string StackTrace { get; set; }
+    }
+}
