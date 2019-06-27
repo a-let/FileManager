0189c5f607acb8d9f608b68be6587e5f91b4d29f
Alex Letinski <aletinski@yahoo.com>
2019-06-26 17:30:34 -0700

    FileManagerContextExtensions in web tests to seed data

--- FileManager.Tests/FileManagerDataAccessLayerTests/DatabaseFixture.cs
+++ FileManager.Tests/FileManagerDataAccessLayerTests/DatabaseFixture.cs
@@ -1,10 +1,8 @@
 ﻿using FileManager.DataAccessLayer;
-using FileManager.DataAccessLayer.Repositories;
-using FileManager.Models;
-using FileManager.Models.Constants;
+
 using Microsoft.EntityFrameworkCore;
+
 using System;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace FileManager.Tests.FileManagerDataAccessLayerTests
@@ -27,104 +25,14 @@ namespace FileManager.Tests.FileManagerDataAccessLayerTests
 
         private async Task InitializeDbForTestsAsync()
         {
-            await SeedUsersAsync();
-            await SeedEpisodesAsync();
-            await SeedMoviesAsync();
-            await SeedSeasonsAsync();
-            await SeedSeriesAsync();
-            await SeedShowsAsync();
+            await Context.SeedUsersAsync();
+            await Context.SeedEpisodesAsync();
+            await Context.SeedMoviesAsync();
+            await Context.SeedSeasonsAsync();
+            await Context.SeedSeriesAsync();
+            await Context.SeedShowsAsync();
 
             await Context.SaveChangesAsync();
-        }
-
-        private async Task SeedEpisodesAsync()
-        {
-            await Context.Episode.AddRangeAsync(new[]
-            {
-                new Episode
-                {
-                    EpisodeId = 0,
-                    EpisodeNumber = 1,
-                    SeasonId = 1,
-                    Format = FileFormatTypes.MKV,
-                    Name = "Test",
-                    Path = "Some Path"
-                }
-            });
-        }
-
-        private async Task SeedSeasonsAsync()
-        {
-            await Context.Season.AddRangeAsync(new[]
-            {
-                new Season
-                {
-                    SeasonId = 0,
-                    ShowId = 1,
-                    SeasonNumber = 1,
-                    Path = "Test"
-                }
-            });
-        }
-
-        private async Task SeedShowsAsync()
-        {
-            await Context.Show.AddRangeAsync(new[]
-            {
-                new Show
-                {
-                    ShowId = 0,
-                    Name = "Test",
-                    Category = "Test",
-                    Path = "Some Path"
-                }
-            });
-        }
-
-        private async Task SeedSeriesAsync()
-        {
-            await Context.Series.AddRangeAsync(new[]
-            {
-                new Series
-                {
-                    SeriesId = 0,
-                    Name = "Test",
-                    Path = "Some Path"
-                }
-            });
-        }
-
-        private async Task SeedMoviesAsync()
-        {
-            await Context.Movie.AddRangeAsync(new[]
-            {
-                new Movie
-                {
-                    MovieId = 0,
-                    SeriesId = 1,
-                    Category = "Test",
-                    IsSeries = false,
-                    Format = FileFormatTypes.MKV,
-                    Name = "Test",
-                    Path = "Some Path"
-                }
-            });
-        }
-
-        private async Task SeedUsersAsync()
-        {
-            await Context.User.AddRangeAsync(new[]
-            {
-                new User
-                {
-                    UserId = 0,
-                    FirstName = "Test",
-                    LastName = "Tester",
-                    UserName = "TTester",
-                    PasswordHash = Encoding.ASCII.GetBytes("TestHash"),
-                    PasswordSalt = Encoding.ASCII.GetBytes("TestSalt")
-                }
-            });
         }
 
         protected virtual void Dispose(bool disposing)
---
+++ FileManager.Tests/FileManagerDataAccessLayerTests/FileManagerContextExtensions.cs
@@ -0,0 +1,102 @@
+﻿using FileManager.DataAccessLayer;
+using FileManager.Models;
+using FileManager.Models.Constants;
+
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileManager.Tests.FileManagerDataAccessLayerTests
+{
+    public static class FileManagerContextExtensions
+    {
+        public static async Task SeedEpisodesAsync(this FileManagerContext context)
+        {
+            await context.Episode.AddRangeAsync(new[]
+            {
+                new Episode
+                {
+                    EpisodeId = 0,
+                    EpisodeNumber = 1,
+                    SeasonId = 1,
+                    Format = FileFormatTypes.MKV,
+                    Name = "Test",
+                    Path = "Some Path"
+                }
+            });
+        }
+
+        public static async Task SeedSeasonsAsync(this FileManagerContext context)
+        {
+            await context.Season.AddRangeAsync(new[]
+            {
+                new Season
+                {
+                    SeasonId = 0,
+                    ShowId = 1,
+                    SeasonNumber = 1,
+                    Path = "Test"
+                }
+            });
+        }
+
+        public static async Task SeedShowsAsync(this FileManagerContext context)
+        {
+            await context.Show.AddRangeAsync(new[]
+            {
+                new Show
+                {
+                    ShowId = 0,
+                    Name = "Test",
+                    Category = "Test",
+                    Path = "Some Path"
+                }
+            });
+        }
+
+        public static async Task SeedSeriesAsync(this FileManagerContext context)
+        {
+            await context.Series.AddRangeAsync(new[]
+            {
+                new Series
+                {
+                    SeriesId = 0,
+                    Name = "Test",
+                    Path = "Some Path"
+                }
+            });
+        }
+
+        public static async Task SeedMoviesAsync(this FileManagerContext context)
+        {
+            await context.Movie.AddRangeAsync(new[]
+            {
+                new Movie
+                {
+                    MovieId = 0,
+                    SeriesId = 1,
+                    Category = "Test",
+                    IsSeries = false,
+                    Format = FileFormatTypes.MKV,
+                    Name = "Test",
+                    Path = "Some Path"
+                }
+            });
+        }
+
+        public static async Task SeedUsersAsync(this FileManagerContext context)
+        {
+            await context.User.AddRangeAsync(new[]
+            {
+                new User
+                {
+                    UserId = 0,
+                    FirstName = "Test",
+                    LastName = "Tester",
+                    UserName = "TTester",
+                    PasswordHash = Encoding.ASCII.GetBytes("TestHash"),
+                    PasswordSalt = Encoding.ASCII.GetBytes("TestSalt")
+                }
+            });
+        }
+    }
+}
